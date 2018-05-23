4ca8ef5ab8dc1b324ecb973bb97de5cdb3d21f6a
Greater24 <39557968+Greater24@users.noreply.github.com>
2018-05-23 15:26:41 +0200

    bin to dez und dez to bin
    Binaerrechner erweitert um dezimalzahlen

--- Semesterarbeit/src/BinaerRechner.java
+++ Semesterarbeit/src/BinaerRechner.java
@@ -15,43 +15,45 @@ public class BinaerRechner {
     int binaerausgabe;
     String bitstring;
 
-    void binrechner() {
+    void bintodezrechner() {
 
         while (auswahl == 0) {
             //Abfrage zum beenden bei Eingabe 0
-            System.out.println("Bitte geben Sie den 32-Bit String ein:");
+            System.out.println("Bitte geben Sie einen binären String ein:");
             bitstring = eingabe.nextLine();
             //Bitlänge zählen
-            int laenge = bitstring.length();
 
-            if (laenge == 32) {
-                // Länge prüfen, falls nicht 32-Bit Fehler melden
-                // falls 0 = Positive Zahl
-                if (bitstring.startsWith("0")) {
+            // Länge prüfen, falls nicht 32-Bit Fehler melden
+            // falls 0 = Positive Zahl
+            if (bitstring.startsWith("0")) {
                 // Bitstring aus Zahl erstellen
-                    binaerzahl1 = Integer.parseInt(bitstring, 2);
-                    System.out.println("Die Zahl lautet:");
-                    System.out.println(binaerzahl1);
-
-                } else {
-
-                    binaerzahl2 = Long.parseLong(bitstring, 2);
-                    //Casting
-                    binaerausgabe = (int) binaerzahl2;
-                    System.out.println("Die Zahl lautet:");
-                    System.out.println(binaerausgabe);
-                }
+                binaerzahl1 = Integer.parseInt(bitstring, 2);
+                System.out.println("Die Zahl lautet:");
+                System.out.println(binaerzahl1);
 
             } else {
-                System.out.println("Eingabe nicht korrekt, bitte 32-Bit String eingeben");
+
+                binaerzahl2 = Long.parseLong(bitstring, 2);
+                //Casting
+                binaerausgabe = (int) binaerzahl2;
+                System.out.println("Die Zahl lautet:");
+                System.out.println(binaerausgabe);
             }
 
-            System.out.println();
-            System.out.println("Zum fohrtfahren wählen Sie 0, zum beenden wählen sie 1");
-            auswahl = beenden.nextInt();
         }
 
-        System.out.println("Programm wird nun beendet");
+        System.out.println();
+        System.out.println("Zum fohrtfahren wählen Sie 0, zum beenden wählen sie 1");
+        auswahl = beenden.nextInt();
+    }
+
+    void deztobinrechner() {
+
+        int dezstring;
+        System.out.println("Bitte geben Sie eine Dezimale Zahl ein:");
+        dezstring = eingabe.nextInt();
+        System.out.println(Integer.toBinaryString(dezstring));
+
     }
 
 }
