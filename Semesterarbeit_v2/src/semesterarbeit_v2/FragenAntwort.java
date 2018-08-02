f4c048c454bf3b6de2cfa304fbd2715bf400c37e
Greater24 <39557968+Greater24@users.noreply.github.com>
2018-08-02 22:04:51 +0200

    Thema eingepflegt. WLan eingepfegt.
    Thema um die Themen erweitert welche mir gerade eingefallen sind. WLAN eingefüght und getestet.

--- Semesterarbeit_v2/src/semesterarbeit_v2/FragenAntwort.java
+++ Semesterarbeit_v2/src/semesterarbeit_v2/FragenAntwort.java
@@ -72,27 +72,27 @@ public class FragenAntwort {
                 }
             }
         }
-            aussageBoolean = aussageBoolean1 ^ aussageBoolean2;
+        aussageBoolean = aussageBoolean1 ^ aussageBoolean2;
 
-            if (aussageBoolean == false) {
-                // positive
-                System.out.println("Gut zu hören " + nameUser + ". Mein Code hat keine Bugs also"
-                        + " gehts mir gut. :) ");
-            } else {
-                //negative
-                System.out.println("Schade aber auch das geht vorüber."
-                        + " Think positiv :) ");
-            }
+        if (aussageBoolean == false) {
+            // positive
+            System.out.println("Gut zu hören " + nameUser + ". Mein Code hat keine Bugs also"
+                    + " gehts mir gut. :) ");
+        } else {
+            //negative
+            System.out.println("Schade aber auch das geht vorüber."
+                    + " Think positiv :) ");
+        }
 
-            System.out.println("So an die Arbeit!");
-            System.out.println("Ich kann dir diverse Fragen"
-                    + " zum Thema Netzwerk beantworten.");
+        System.out.println("So an die Arbeit!");
+        System.out.println("Ich kann dir diverse Fragen"
+                + " zum Thema Netzwerk beantworten.");
 
-            System.out.println("Ich kann dir auch ein Auflistung mit den mir"
-                    + " bekannten Themen geben.");
-            System.out.println("Sag dafür nur Thema.");
+        System.out.println("Ich kann dir auch ein Auflistung mit den mir"
+                + " bekannten Themen geben.");
+        System.out.println("Sag dafür nur Thema.");
 
-            /*
+        /*
         
         Boolean hasPositives;
         Boolean hasNegatives;
@@ -116,7 +116,7 @@ public class FragenAntwort {
                 break;
   
         
-             */
+         */
 
  /*  switch (wohlbefindenUser) {
             case "gut":
@@ -136,87 +136,109 @@ public class FragenAntwort {
             }
             
         }*/
-        }
+    }
 
-        void chatPartner
-        
-            () {
-        
-        System.out.println("Wie kann ich dir helfen?");
+    void chatPartner() {
 
-            chatUser = mEingabe.nextLine();
-            String[] words = chatUser.split(" ");
-            String[] binrechnerArray = new String[]{"umrechnen", "binaer", "binaer", "dezimal"};
-            String[] subnetingArray = new String[]{"Subnetz", "IP", "IP Adresse", "Gateway", "Broadcast"};
-            String[] themaArray = new String[]{"thema", "wissen", "weisst", "sagen"};
-            String[] beendenArray = new String[]{"fertig", "exit", "quit", "cancel", "schluss", "schliessen", "schliesse", "aufhoeren", "beenden"};
+        System.out.println("Wie kann ich dir helfen " + nameUser + "?");
 
-            for (String word : words) {
-                for (String binrechner : binrechnerArray) {
-                    if (word.contains(binrechner)) {
-                        System.out.println("Sehr gut, zum Thema Binaer und"
-                                + " und Dezimalrechner kann ich dir Auskunft"
-                                + " geben.");
-                        binrechnerMenu();
-                    }
+        chatUser = mEingabe.nextLine();
+        String[] words = chatUser.split(" ");
+        String[] binrechnerArray = new String[]{"umrechnen", "binaer", "dezimal", "rechner"};
+        String[] subnetingArray = new String[]{"Subnetz", "IP", "IP Adresse", "Gateway", "Broadcast"};
+        String[] themaArray = new String[]{"thema", "wissen", "weisst", "sagen"};
+        String[] beendenArray = new String[]{"fertig", "exit", "quit", "cancel", "schluss", "schliessen", "schliesse", "aufhoeren", "beenden"};
+        String[] wlanArray = new String[]{"wifi", "wlan", "ghz", "drathlos"};
+
+        for (String word : words) {
+            for (String binrechner : binrechnerArray) {
+                if (word.contains(binrechner)) {
+                    System.out.println("Sehr gut, zum Thema Binaer und"
+                            + " und Dezimalrechner kann ich dir Auskunft"
+                            + " geben " + nameUser + ".");
+                    binrechnerMenu();
                 }
-                for (String subrechner : subnetingArray) {
-                    if (word.contains(subrechner)) {
-                        System.out.println("Aber sicher doch " + nameUser + ". Ich kann dir mit"
-                                + " einem Subnetz und IP Rechner aushelfen.");
-                        // HIER SUBNET RECHNER INTEGRIEREN
-                    }
-                }
-
-                for (String thema : themaArray) {
-                    if (word.contains(thema)) {
-                        System.out.println("Alles klar " + nameUser + ".");
-                        System.out.println("Ich kann dir zu folgenden Themen Auskunft geben.");
-
-                    }
-                }
-                for (String beenden : beendenArray) {
-                    if (word.contains(beenden)) {
-                        System.out.println("Ich hoffe ich konnte dir helfen " + nameUser + ".");
-                        System.out.println("Ich schliesse mich nun für dich.");
-                        System.out.println("Bye bye!");
-                        System.exit(0);
-
-                    }
+            }
+            for (String subrechner : subnetingArray) {
+                if (word.contains(subrechner)) {
+                    System.out.println("Aber sicher doch " + nameUser + ". Ich kann dir mit"
+                            + " einem Subnetz und IP Rechner aushelfen.");
+                    // HIER SUBNET RECHNER INTEGRIEREN
                 }
             }
 
+            for (String thema : themaArray) {
+                if (word.contains(thema)) {
+                    System.out.println("Alles klar " + nameUser + ".");
+                    System.out.println("Da ich leider noch keinen Zugriff zum Internet habe ist meine Auswahl beschränkt. "
+                            + " Aber zu diesen Themen kann ich dir helfen.");
+                    System.out.println("-------------------");
+                    System.out.println("Netzwerk Generell--");
+                    System.out.println("Subnetzrechner-----");
+                    System.out.println("Topologien---------");
+                    System.out.println("Binärrechner-------");
+                    System.out.println("Firewall-----------");
+                    System.out.println("Switches-----------");
+                    System.out.println("Router-------------");
+                    System.out.println("WLAN---------------");
+                    System.out.println("LAN----------------");
+                    System.out.println("-------------------");
+                    System.out.println("Und vieleicht noch etwas mehr frag mich doch einfach :) ");
+
+                }
+            }
+            for (String wlan : wlanArray) {
+                if (word.contains(wlan)) {
+                    System.out.println("WLAN steht für Wireless Local Area Network.");
+                    System.out.println("Hiermit ist eine 2.4 GHZ oder 5 GHZ Funkverbindung gemeint, mit welcher Internet ohne");
+                    System.out.println("auf ein Kabel angewiesen zu sein zu einem Gerät übertragen werden kann.");
+                    System.out.println("Wi-Fi ist ein für Vermarktungszwecke erfundener Kunstbegriff,");
+                    System.out.println("der in Analogie zu Hi-Fi gebildet wurde.");
+                    System.out.println("Er diente dafür um das freie WLAN attraktiv zu vermarkten.");
+                           
+
+                }
+            }
+            for (String beenden : beendenArray) {
+                if (word.contains(beenden)) {
+                    System.out.println("Ich hoffe ich konnte dir helfen " + nameUser + ".");
+                    System.out.println("Ich schliesse mich nun für dich.");
+                    System.out.println("Bye bye!");
+                    System.exit(0);
+
+                }
+            }
         }
 
-        void binrechnerMenu
-        
-            () {
+    }
+
+    void binrechnerMenu() {
 
         System.out.println("Für den Binärrechner gibt es 2 Modi.");
-            System.out.println("Einmal von Binär nach Dezimal und Dezimal zu"
-                    + " Binär.");
-            System.out.println("Welchen darf ich für dich starten? Binaer oder Dezimal?");
-            chatUser = mEingabe.nextLine();
-            String[] words = chatUser.split(" ");
-            String[] binDezArray = new String[]{"binaer"};
-            String[] dezBinArray = new String[]{"dezimal"};
+        System.out.println("Einmal von Binär nach Dezimal und Dezimal zu"
+                + " Binär.");
+        System.out.println("Welchen darf ich für dich starten? Binaer oder Dezimal?");
+        chatUser = mEingabe.nextLine();
+        String[] words = chatUser.split(" ");
+        String[] binDezArray = new String[]{"binaer"};
+        String[] dezBinArray = new String[]{"dezimal"};
 
-            for (String word : words) {
-                for (String binDezRechner : binDezArray) {
-                    if (word.contains(binDezRechner)) {
-                        System.out.println("Ich starte für dich den Binär zu "
-                                + "Dezimal Rechner");
-                        bin1.bintoDezRechner();
-                    }
-                }
-                for (String dezBinRechner : dezBinArray) {
-                    if (word.contains(dezBinRechner)) {
-                        System.out.println("Ich starte für dich den Dezimal zu "
-                                + "Binär Rechner");
-                        bin1.deztoBinRechner();
-                    }
+        for (String word : words) {
+            for (String binDezRechner : binDezArray) {
+                if (word.contains(binDezRechner)) {
+                    System.out.println("Ich starte für dich den Binär zu "
+                            + "Dezimal Rechner");
+                    bin1.bintoDezRechner();
                 }
             }
-
+            for (String dezBinRechner : dezBinArray) {
+                if (word.contains(dezBinRechner)) {
+                    System.out.println("Ich starte für dich den Dezimal zu "
+                            + "Binär Rechner");
+                    bin1.deztoBinRechner();
+                }
+            }
         }
+
     }
+}
