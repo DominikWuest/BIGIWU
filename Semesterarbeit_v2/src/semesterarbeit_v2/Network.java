eae3134db1c882c7f9e76b1cfc127ba6e50bd132
Greater24 <39557968+Greater24@users.noreply.github.com>
2018-08-13 09:50:38 +0200

    Klassen ausgefüllt
    Klassen mit Text ausgefüllt.

--- Semesterarbeit_v2/src/semesterarbeit_v2/Network.java
+++ Semesterarbeit_v2/src/semesterarbeit_v2/Network.java
@@ -12,7 +12,18 @@ package semesterarbeit_v2;
 public class Network {
     
     void detaliertNetwork (){
-        
+        System.out.println("Generells über Netzwerke.");
+        System.out.println("Das Internet ist nichts weiter als ein gigantisches Netzwerk.");
+        System.out.println("Dieses Verbindet die ganze Welt miteinander.");
+        System.out.println("Ein Netzwerk wird grob gesagt in ca. 4 Klassen eingestuft.");
+        System.out.println("LAN, MAN, WAN, GAN");
+        System.out.println("Local Area Network: Heimnetzwerk oder kleines Unternehmensnetzwerk");
+        System.out.println("Metropolian Area Network: Ein Netzwerk welches ca. über 10 km reicht.");
+        System.out.println("Wide Area Network: Wie MAN aber mit einer weiteren geografischen Ausbreitung.");
+        System.out.println("WANs erstrecken sich über Länder oder sogar Kontinente.");
+        System.out.println("Globale Area Networks: Verbindetet mehere WANs miteinander. Hier kommen auch Satiliten zum Einsatz.");
+        System.out.println("Netzwerke dienen dazu, Clients oder andere Netzwerke miteinander zu verbinden.");
+        System.out.println("Sie sind der Grundstein für die Kommunikation wie wir sie heute kennen und nutzen.");
     }
     
 }
