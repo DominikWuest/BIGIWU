098ab5a59a8229f0a20c08866b72b09a9b1744cc
Greater24 <39557968+Greater24@users.noreply.github.com>
2018-08-19 19:19:38 +0200

    NoMatch verbessert
    Code halbiert mit Funktion. Immer noch etwas lang aber habe keine Zeit mehr um Funktion auszulagern. Wenn noch zeit dann mach ich das.

--- Semesterarbeit_v2/src/semesterarbeit_v2/NoMatch.java
+++ Semesterarbeit_v2/src/semesterarbeit_v2/NoMatch.java
@@ -28,49 +28,42 @@ public class NoMatch {
 
         this.question = question;
 
-        System.out.println("Ich habe deine Eingabe leider nicht verstanden.");
-        System.out.println("Du kannst gerne es nochmals probieren, oder ich starte Google Search für dich.");
+        System.out.println("Ich habe deine Eingabe leider "
+                + "nicht verstanden.");
+        System.out.println("Du kannst gerne es nochmals probieren, "
+                + "oder ich starte Google Search für dich.");
         System.out.println("Suche starten oder nochmals eingeben?");
 
         sucheAntwort = mEingabe.nextLine();
         String[] words = sucheAntwort.split(" ");
 
-        String[] nochmalsArray = new String[]{"nochmals", "vertippt", "noch", "mal", "typo", "normal", "bot", "eingeben"};
-        String[] googleArray = new String[]{"google", "suche", "internet", "search", "starten"};
+        String[] nochmalsArray = new String[]{"nochmals", "vertippt",
+            "noch", "mal", "typo", "normal", "bot", "eingeben"};
+        String[] googleArray = new String[]{"google", "suche",
+            "internet", "search", "starten"};
 
-        // Jedes Wort wird geprüft und die Booleans geändert
-        for (String word : words) {
-            // prüft ob positiv vorhanden
-            for (String nochmals : nochmalsArray) {
-                if (word.equalsIgnoreCase(nochmals)) {
-                    System.out.println("Dann nochmals.");
-                    
-                }
+        if (Utilities.uebereinstimungen(words, nochmalsArray)) {
+            System.out.println("Dann nochmals.");
+        } else if (Utilities.uebereinstimungen(words, googleArray)) {
+            System.out.println("Ich starte die Google Suche.");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+
+            //String question = "Wetter heute";
+            question = question.replace(' ', '+');
+
+            try {
+                Runtime.getRuntime().exec(new String[]{"cmd", "/c", 
+                "start chrome https://www.google.com/search?q=" 
+                + question});
+            } catch (Exception e) {
+
             }
         }
-        for (String word : words) {
-            // prüft ob negativ vorhanden
-            for (String google : googleArray) {
-                if (word.equalsIgnoreCase(google)) {
-                    System.out.println("Ich starte die Google Suche.");
-                    try {
-                        Thread.sleep(3000);
-                    } catch (InterruptedException ex) {
-                        Thread.currentThread().interrupt();
-                    }
-
-                    //String question = "Wetter heute";
-                    question = question.replace(' ', '+');
-
-                    try {
-                        Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start chrome https://www.google.com/search?q=" + question});
-                    } catch (Exception e) {
-
-                    }
-                }
-            }
-        }
-
+        
     }
 
 }
