ba6f92ab142b52801401c0fd33bb777ced2cc66e
Greater24 <39557968+Greater24@users.noreply.github.com>
2018-08-23 18:10:03 +0200

    StringArray
    Stringarray implementiert.
    Nun alle Arrays in eigener klasse spart ca. 200 zeilen code

---
+++ Semesterarbeit_v2/src/semesterarbeit_v2/StringArrays.java
@@ -0,0 +1,78 @@
+/*
+    Semesterarbeit "Objektorientierter Chatbot" 
+    Schule:     ABB Technikerschule Baden
+    Semester:   4
+    Klasse:     B18-if4.1
+    Authoren:
+                Dominik Wüst
+                Andri Binda
+                Marek Giesen
+    
+ */
+
+ /*
+    Klasse mit den Arrays
+    Hier werden alle Arrays gespeichert um den restlichen Code schlanker
+    zu halten. Arrays können in allen Classen verwendet werden.
+    Alle Darstellungen für A4 Ausdruck zusammengerückt.
+ */
+package semesterarbeit_v2;
+
+public class StringArrays {
+
+    public static String[] binrechnerArray = new String[]{"binaer", "dezimal", "null",
+        "eins", "nullen"};
+
+    public static String[] subnetingArray = new String[]{"subnetz", "ip", "adresse",
+        "adressen", "gateway", "broadcast", "subnetting",
+        "ipadressen", "ipadresse", "subnet", "subnetzrechner",
+        "subnetzmaske", "maske", "iprechner", "ipadresse", "ipadressen",
+        "gatewayadresse", "broadcastadresse"};
+
+    public static String[] themaArray = new String[]{"thema", "katalog",
+        "themas", "themen", "wissensdatenbank"};
+
+    public static String[] firewallArray = new String[]{"firewall", "ports",
+        "trunk", "sicherheit", "blockieren"};
+
+    public static String[] wlanArray = new String[]{"wifi", "wlan", "ghz", "drathlos"};
+
+    public static String[] nameArray = new String[]{"name", "heisst",
+        "gigachatbot3000", "bigiwu"};
+
+    public static String[] topologieArray = new String[]{"topologie", "stern",
+        "daisychain", "bus", "mesh", "line", "zusammenschliessen",
+        "token", "ring", "tokenring", "tree"};
+
+    public static String[] lanArray = new String[]{"lan", "ethernet", "cat",
+        "drahtgebundene", "patch", "rj45", "rj-45"};
+
+    public static String[] switchArray = new String[]{"switch", "port",
+        "ethernetconnection", "verbindung", "networkswitch"};
+
+    public static String[] routerArray = new String[]{"router", "internetverbindung",
+        "internet", "anbieter", "provider"};
+
+    public static String[] networkArray = new String[]{"netzwerk", "network", "generell",
+        "allgemein"};
+
+    public static String[] wetterArray = new String[]{"wetter", "sonne", "regen",
+        "schnee", "kalt", "warm", "regenschirm", "jacke",
+        "schneit", "regnet"};
+
+    public static String[] wohlbefindenBotArray = new String[]{"geht", "fuehlst",
+        "gefuehle"};
+
+    public static String[] witzArray = new String[]{"witz", "lustig", "lachen"};
+
+    public static String[] infoArray = new String[]{"urheber", "copyright",
+        "impressum", "about"};
+
+    public static String[] schimpfwortArray = new String[]{"scheisse", "bloed", "doof",
+        "mist", "dumm"};
+
+    public static String[] beendenArray = new String[]{"fertig", "exit", "quit",
+        "cancel", "schluss", "schliessen", "aufhoeren", "beenden",
+        "ciao", "adios", "bye"};
+
+}
